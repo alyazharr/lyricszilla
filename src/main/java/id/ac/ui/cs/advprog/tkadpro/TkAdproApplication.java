95a2cd88439625e870143348e6b20d36cc789c1f
Sulthan Afif Althaf <afifalthafs1@gmail.com>
2022-04-12 13:26:59 +0700

    initial setup

---
+++ src/main/java/id/ac/ui/cs/advprog/tkadpro/TkAdproApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.tkadpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TkAdproApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TkAdproApplication.class, args);
+    }
+
+}
