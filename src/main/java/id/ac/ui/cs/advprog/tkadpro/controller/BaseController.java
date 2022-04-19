3ca11e6e3870f621bcfbd4630212bce8741617e5
Alya Azhar Agharid <alya.azhar@ui.ac.id>
2022-04-19 15:42:16 +0700

    Creating all base game frontend

--- src/main/java/id/ac/ui/cs/advprog/tkadpro/controller/BaseController.java
+++ src/main/java/id/ac/ui/cs/advprog/tkadpro/controller/BaseController.java
@@ -2,9 +2,8 @@ package id.ac.ui.cs.advprog.tkadpro.controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
@@ -14,8 +13,74 @@ public class BaseController {
     @RequestMapping(path="/login", method = RequestMethod.GET)
     public String loginPage(){return "login/login";}
 
-    @RequestMapping(path="/", method=RequestMethod.GET)
+    @RequestMapping(path="", method=RequestMethod.GET)
     public String homePage(){return "homepage/homepage";}
 
+    @RequestMapping(path="/wordsblank", method=RequestMethod.GET)
+    public String Wordsblank(Model model){
+        int num = 3;
+        int score = 53;
+        int numOfAns = 1;
+        String level = "EASY";
+        String text = "Implementing lyrics here";
 
+        model.addAttribute("numOfQuest", num);
+        model.addAttribute("score", score);
+        model.addAttribute("ans", numOfAns);
+        model.addAttribute("level", level);
+        model.addAttribute("txt", text);
+
+        return "wordsblank/base_wordsblank";
+    }
+
+    @RequestMapping(path="/titleque", method=RequestMethod.GET)
+    public String Titleque(Model model){
+        int num = 18;
+        int score = 97;
+        int numOfAns = 5;
+        String level = "HARD";
+        String text = "Implementing lyrics here";
+
+        model.addAttribute("numOfQuest", num);
+        model.addAttribute("score", score);
+        model.addAttribute("ans", numOfAns);
+        model.addAttribute("level", level);
+        model.addAttribute("txt", text);
+
+        return "titleque/base_titleque";
+    }
+
+    @RequestMapping(path="/starguess", method=RequestMethod.GET)
+    public String Starguess(Model model){
+        int num = 6;
+        int score = 53;
+        int numOfAns = 3;
+        String level = "MEDIUM";
+        String text = "Implementing lyrics here";
+
+        model.addAttribute("numOfQuest", num);
+        model.addAttribute("score", score);
+        model.addAttribute("ans", numOfAns);
+        model.addAttribute("level", level);
+        model.addAttribute("txt", text);
+
+        return "starguess/base_starguess";
+    }
+
+    @RequestMapping(path="/lyricspatch", method=RequestMethod.GET)
+    public String Lyricspatch(Model model){
+        int num = 13;
+        int score = 78;
+        int numOfAns = 5;
+        String level = "HARD";
+        String text = "Implementing lyrics here";
+
+        model.addAttribute("numOfQuest", num);
+        model.addAttribute("score", score);
+        model.addAttribute("ans", numOfAns);
+        model.addAttribute("level", level);
+        model.addAttribute("txt", text);
+
+        return "lyricspatch/base_lyricspatch";
+    }
 }
