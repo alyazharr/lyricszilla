beb6495e261f6001f1e9484f9dad9c39da707330
ErickEzrandy274 <erick.ezrandy@ui.ac.id>
2022-04-22 11:44:10 +0700

    change datasource into postgre heroku and create Lyricpatch file

---
+++ src/main/java/id/ac/ui/cs/advprog/tkadpro/core/GameType/Lyricspatch.java
@@ -0,0 +1,35 @@
+package id.ac.ui.cs.advprog.tkadpro.core.GameType;
+
+import id.ac.ui.cs.advprog.tkadpro.core.Level;
+import id.ac.ui.cs.advprog.tkadpro.core.tools.BlankFeature;
+import id.ac.ui.cs.advprog.tkadpro.core.tools.BlankFeatureImpl;
+import id.ac.ui.cs.advprog.tkadpro.rest.SongDTO;
+
+import java.util.List;
+
+public class Lyricspatch extends GameType {
+    BlankFeature blankFeature;
+
+    public Lyricspatch(SongDTO[] allSongs) {
+        super(allSongs);
+        blankFeature = new BlankFeatureImpl();
+    }
+
+    @Override
+    public List<String> getEasyQnA() {
+        var song = allSongs[random.nextInt(allSongs.length)];
+        return blankFeature.generateBlankLine(song, Level.EASY);
+    }
+
+    @Override
+    public List<String> getMediumQnA() {
+        var song = allSongs[random.nextInt(allSongs.length)];
+        return blankFeature.generateBlankLine(song, Level.MEDIUM);
+    }
+
+    @Override
+    public List<String> getHardQnA() {
+        var song = allSongs[random.nextInt(allSongs.length)];
+        return blankFeature.generateBlankLine(song, Level.HARD);
+    }
+}
