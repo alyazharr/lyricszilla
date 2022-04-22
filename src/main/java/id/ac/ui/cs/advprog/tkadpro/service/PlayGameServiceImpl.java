541cd24c262bd096c598283f8c509e0d25309cba
Sulthan Afif Althaf <afifalthafs1@gmail.com>
2022-04-22 23:11:04 +0700

    Refactor PlayGameService

--- src/main/java/id/ac/ui/cs/advprog/tkadpro/service/PlayGameServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/tkadpro/service/PlayGameServiceImpl.java
@@ -2,6 +2,7 @@ package id.ac.ui.cs.advprog.tkadpro.service;
 
 import id.ac.ui.cs.advprog.tkadpro.core.GameLevel.PlayGame;
 import id.ac.ui.cs.advprog.tkadpro.core.GameType.TypeGame;
+import id.ac.ui.cs.advprog.tkadpro.model.QuestionInfo;
 import id.ac.ui.cs.advprog.tkadpro.repository.GameTypeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,7 +18,7 @@ public class PlayGameServiceImpl implements PlayGameService {
     private GameTypeRepository gameTypeRepository;
 
     @Override
-    public String startGame(TypeGame typeGame) {
+    public QuestionInfo startGame(TypeGame typeGame) {
         playGame.setFinished(false);
         playGame.setCurrentState(playGame.easyLevelState);
         playGame.setGameType(gameTypeRepository.findByType(typeGame));
@@ -26,16 +27,16 @@ public class PlayGameServiceImpl implements PlayGameService {
     }
 
     @Override
-    public String generateQuestion() {
+    public QuestionInfo generateQuestion() {
         String question = null;
         if(!playGame.isFinished()){
             question = playGame.play();
         }
-        else {
-            question = "NO QUESTION LEFT";
-        }
 
-        return question;
+        var questionInfo = new QuestionInfo(playGame.getQuestionCounter(), playGame.getPoints(),
+                playGame.getNumberOfAnswer(), playGame.getCurrentState().toString(), question);
+
+        return questionInfo;
     }
 
     @Override
