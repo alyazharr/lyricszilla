87069424b875ae76671957e4ac500ed43489f802
Debra Mazaya <debra.mazaya@ui.ac.id>
2022-04-25 19:58:22 +0700

    Added testcases for rest and repository

--- src/test/java/id/ac/ui/cs/advprog/tkadpro/rest/SongDTOTest.java
+++ src/test/java/id/ac/ui/cs/advprog/tkadpro/rest/SongDTOTest.java
@@ -1,4 +1,28 @@
+package id.ac.ui.cs.advprog.tkadpro.rest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Modifier;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SongDTOTest {
-  
+    private Class<?> songDTOClass;
+    @BeforeEach
+    public void setUp() throws Exception {
+        songDTOClass = Class.forName("id.ac.ui.cs.advprog.tkadpro.rest.SongDTO");
+    }
+
+    @Test
+    public void testSongDTOIsNotAbstractClass() {
+        assertFalse(Modifier.isAbstract(songDTOClass.getModifiers()));
+    }
+
+    @Test
+    public void testSongDTOIsPublic() {
+        int classModifiers = songDTOClass.getModifiers();
+
+        assertTrue(Modifier.isPublic(classModifiers));
+    }
 }
